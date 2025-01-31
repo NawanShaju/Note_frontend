62683e10288a738b663b35a6e5aa83e18ffee4aa
Nawan <z5527177@ad.edu.unsw.au>
2025-01-31 18:16:57 +0800

    added deployed api url

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://127.0.0.1:5000/"
+const BASE_URL = "https://note-backend-8oe1.onrender.com/";
 
 export const getNotes = async() => {
     try {
